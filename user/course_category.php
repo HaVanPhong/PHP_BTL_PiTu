30170a8e292647aa9c7cb215f003ce0b92ecc0eb
unknown <haphonggoroki@gmail.com>
2022-07-01 00:37:07 +0700

    timkiem khoahoc xong

--- user/course_category.php
+++ user/course_category.php
@@ -1,3 +1,13 @@
+<style>
+#formSearch input{
+    padding: 10px;
+    width: 500px;
+    border-radius: 12px;
+    font-size: 20px;
+}
+
+</style>
+
 <?php
     // GET id là lấy id từ bên MENU.php 
 
@@ -9,11 +19,19 @@
         $sql_show ="SELECT * FROM course ORDER BY courseName DESC";
         $query_show =mysqli_query($connect,$sql_show);
     }
+
+    if (isset($_POST['search'])){
+        $sql_show="SELECT * FROM course where courseName like '%".$_POST['search']."%'";
+        $query_show= mysqli_query($connect, $sql_show);
+    }
     
 ?>
 
 <div class="list_course_category">
 <div class="list" align="center">
+<form action="" id="formSearch" method="POST" enctype="multipart/form-data">
+        <input type="text" placeholder="Tìm kiếm khóa học" name="search" id="inpSearch">
+</form>
 <ul class="course_list">
     <?php
         while($row_pro=mysqli_fetch_array($query_show)){
@@ -30,3 +48,14 @@
 
 </div>
 </div>
+<script>
+    let inpSearch= document.getElementById("inpSearch")
+    let formSearch= document.getElementById("formSearch")
+    inpSearch.addEventListener("keypress", function(event){
+        if (event.key==="Enter"){
+            event.preventDefault();
+            console.log(inpSearch.value)
+            formSearch.submit()
+        }
+    })
+</script>
